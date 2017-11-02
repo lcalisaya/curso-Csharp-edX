9cb0de57a659566f35f580c23418abc4b3adb658
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-02 17:24:03 -0300

    Se guardan dos ejemplos de estructuras de repeticiónwhile y do-while

---
+++ Mod2_Lab5/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+
+namespace Mod2_Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char cruz = 'X';
+            char circulo = 'O';
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write(cruz);
+                        }
+                        else
+                        {
+                            Console.Write(circulo);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write(circulo);
+                        }
+                        else
+                        {
+                            Console.Write(cruz);
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
