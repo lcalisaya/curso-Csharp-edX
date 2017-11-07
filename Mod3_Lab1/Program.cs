c538032784d6c06aed976098a092b028bbe0291c
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-07 19:19:03 -0300

    Se sobrecarga el método Sum() de tres maneras

--- Mod3_Lab1/Program.cs
+++ Mod3_Lab1/Program.cs
@@ -4,10 +4,18 @@ namespace Mod3_Lab1
 {
     class Program
     {
+        // How method overloading works?
+        // Even though they are all 3 named Sum, the compiler works out the correct method to call based on the method signature.
         static void Main(string[] args)
         {
             int result = Sum(20, 40);
-            Console.WriteLine($"La suma de 20 y 40 es {result}");
+            Console.WriteLine($"Llamando a Sum() con dos argumentos, el resultado es: {result}");
+
+            int result3 = Sum(10, 50, 80);
+            Console.WriteLine($"Llamando a Sum() con tres argumentos, el resultado es: {result3}");
+
+            double dblResult = Sum(20.5, 30.6);
+            Console.WriteLine($"Llamando a Sum() que toma tipos de datos double, el resultado es: {dblResult}");
         }
 
         // Sum() method that takes two integer arguments and sums them
@@ -18,6 +26,27 @@ namespace Mod3_Lab1
         {
             int sum = first + second;
             return sum;
-        }       
+        }
+
+        // Sum() method that takes three integer arguments and sums them
+        // and then returns the value
+        // This method uses the same name as the Sum() method that takes two integers
+        // but the parameters here indicate the method is expecting three integers as arguments
+        // The compiler knows which method to call based on the number of arguments passed in
+        static int Sum(int first, int second, int third)
+        {
+            int sum = first + second + third;
+            return sum;
+        }
+
+        // Sum() method that takes two doubles as arguments
+        // This method uses the same name as the Sum() method that takes two integers
+        // but the parameters here indicate the method is expecting two doubles as arguments
+        // The compiler knows which method to call based on the arguments data types
+        static double Sum(double first, double second)
+        {
+            double result = first + second;
+            return result;
+        }
     }
 }
