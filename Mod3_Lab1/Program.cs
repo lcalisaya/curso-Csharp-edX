1949de4d69cb86911f19652173b519072f6526c9
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-07 19:03:19 -0300

    Se agrega un método que suma dos números

---
+++ Mod3_Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mod3_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Sum(20, 40);
+            Console.WriteLine($"La suma de 20 y 40 es {result}");
+        }
+
+        // Sum() method that takes two integer arguments and sums them
+        // The method returns no value which is why we use void
+        // We also need to use static in the method signature because Main is static
+        // and you cannot call a non-static method from a static method
+        static int Sum(int first, int second)
+        {
+            int sum = first + second;
+            return sum;
+        }       
+    }
+}
