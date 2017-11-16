2c8ed99bca8759d060a0bce837d072245aed45b5
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-16 13:16:22 -0300

    Se ejecuta la excepción System.DivideByZeroException al intentar dividir con divisor=0

---
+++ Mod3_Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mod3_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Divide(2, 0);
+        }
+        
+        // Method Divide() accepts two integer arguments and returns the result
+        // of dividing first by second
+        // We do no validation of the the arguments so a user may pass in a zero
+        // in place of the second parameter. This is an illegal operation in Math
+        // If we don't validate input or catch the error, the application will crash
+        static int Divide(int first, int second)
+        {
+            int result = first / second;
+            return result;
+        }
+    }
+}
