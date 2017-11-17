79d75a5f4171ed7adb5e2ae75e4a90d18d1adae0
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-17 14:26:32 -0300

    Se agregan los métodos que obtienen los datos de entrada

--- Mod3_Lab3/Program.cs
+++ Mod3_Lab3/Program.cs
@@ -15,9 +15,51 @@ namespace Mod3_Lab3
             string firstName = Console.ReadLine();
             Console.WriteLine("Ingrese el Apellido del Estudiante:");
             string lastName = Console.ReadLine();
-            // Code to finish getting the rest of the student data
-            .....
+            Console.WriteLine("Ingrese fecha de nacimiento del Estudiante con formato dd/mm/aaaa:");
+            DateTime birthDate = Convert.ToDateTime(Console.ReadLine());
         }
+
+        static void GetTeacherInformation()
+        {
+            Console.WriteLine("Ingrese en Nombre del Profesor:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Ingrese el Apellido del Profesor:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Ingrese fecha de nacimiento del Profesor con formato dd/mm/aaaa:");
+            DateTime birthDate = Convert.ToDateTime(Console.ReadLine());
+        }
+
+        static void GetCourseInformation()
+        {
+            Console.WriteLine("Ingrese el nombre del curso:");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("Ingrese los créditos del curso:");
+            string credits = Console.ReadLine();
+            Console.WriteLine("Ingrese la duración en semanas del curso:");
+            int	durationInweeks = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el profesor del curso:");
+            string teacher = Console.ReadLine();
+        }
+
+        static void GetUProgramInformation()
+        {
+            Console.WriteLine("Ingrese el nombre del programa:");
+            string programName = Console.ReadLine();
+            Console.WriteLine("Ingrese el jefe del departamento:");
+            string departmentHead = Console.ReadLine();
+            Console.WriteLine("Ingrese los títulos:");
+            string degrees = Console.ReadLine();
+        }
+
+        static void GetDegreeInformation()
+        {
+            Console.WriteLine("Ingrese el nombre del título:");
+            string degreeName = Console.ReadLine();
+            Console.WriteLine("Ingrese los créditos requeridos:");
+            bool creditsRequired = Console.ReadLine();
+        }
+            
+           
 
         static void PrintStudentDetails(string first, string last, string birthday)
         {
