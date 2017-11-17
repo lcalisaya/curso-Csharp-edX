c0280bb33ca7c2310795b8a4b9cfebaf1e7ef240
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-17 14:03:19 -0300

    Inicio de una autoevaluación: métodos básicos para implementar

---
+++ Mod3_Lab3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mod3_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void GetStudentInformation()
+        {
+            Console.WriteLine("Ingrese en Nombre del Estudiante:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Ingrese el Apellido del Estudiante:");
+            string lastName = Console.ReadLine();
+            // Code to finish getting the rest of the student data
+            .....
+        }
+
+        static void PrintStudentDetails(string first, string last, string birthday)
+        {
+            Console.WriteLine("{0} {1} nació el: {2}", first, last, birthday);
+        }
+    }
+}
